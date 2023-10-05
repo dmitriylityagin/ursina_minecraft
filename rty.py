95128068868a5ec1c6f1b43488d6fb167c322e24
dmitriylityagin <124432694+dmitriylityagin@users.noreply.github.com>
2023-10-05 21:26:28 +0300

    Add files via upload

--- rty.py
+++ rty.py
@@ -2,37 +2,70 @@ from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 # from numpy import *
 from perlin_noise import PerlinNoise
-import random
+from random import *
 
 app = Ursina()
+
+player = FirstPersonController()
+player.cursor.visible = False
+player.gravity = 0.65
+player.y = 20
+
 window.fullscreen_resolution = (1920, 1080)
+window.exit_button.visible = False
 window.fullscreen = True
-gug = load_texture("ura.png")
-Zombie = load_texture("zombie.png")
+gug = "grass.png"
+# Zombie = "zombie.png"
+frameTex = 'frame.png'
+block = load_model("assets/cubik.obj")
+frame = Entity(model=block, texture=frameTex)
+
+
+class BTYPE:
+    Grass = 'grass.png'
+    GrassModel = 'cubik.obj'
+    Dirt = 'blocks/dirt.png'
+    Wood = 'blocks/wood.png'
+    Leaves = 'blocks/leave.png'
+    Sand = 'blocks/sand.png'
+    Bricks = 'blocks/brick.png'
+
+
+Btype = BTYPE.Grass
+Mtype = BTYPE.GrassModel
 
 
 def input(key):
     if key == "escape":
         quit()
+    if key == 'right mouse up':
+        e = duplicate(frame)
+        e.collider = 'mesh'
+        e.model = Mtype
+        e.texture = Btype
 
 
 def update():
     if player.y < -3:
         player.y = 10
-    zombie1.look_at(player, 'back')
-    zombie1.rotation_x = 0
+
+    frame.position = floor(player.position + camera.forward * 5)
+    frame.y = frame.y + 2
+    # zombie1.look_at(player, 'back')
+    # zombie1.rotation_x = 0
     genTerr()
 
 
-noise = PerlinNoise(octaves=2, seed=random.randint(1, 9999999))
-amp = 6
-freq = amp * 4
+noise = PerlinNoise(octaves=2, seed=randrange(1, 9999999999999999999999999999999999999))
+amp = 0
+freq = 24
 
 shells = []
-shellWidth = 20
+shellWidth = 16
 
-for i in range(shellWidth*shellWidth):
-    ent = Entity(model='cube', texture=gug, collider='box')
+for i in range(shellWidth * shellWidth):
+    cube = load_model("assets/cubik.obj")
+    ent = Entity(model=cube, texture=gug, collider='box')
     shells.append(ent)
 
 
@@ -44,22 +77,7 @@ def genTerr():
         y = shells[i].y = floor(noise([x / freq, z / freq]) * amp)
 
 
-# for i in range(dist * dist):
-#     cube = Entity(model='cube', texture=gug, collider="mesh")
-#     cube.x = floor(i / dist)
-#     cube.z = floor(i % dist)
-#     cube.y = floor(noise([cube.x / freq, cube.z / freq]) * amp)
 # ZombieModel = load_model('zombie.obj')
-# zombie1 = Entity(model=ZombieModel, texture=Zombie, scale=1, x=dist / 2, y=0.5, z=dist / 2)
-# zombie1.gravity = 0.65
-# for x in range(0, 15):
-#     for y in range(0, 15):
-#         Entity(model='cube', texture=gug, collider="mesh", scale=(1, 1, 1), position=(x, 0, y))
-# # cube = Entity(model='cube', texture=gug, collider="mesh", scale=(2, 2, 2), position=(1, 0, 5))
-player = FirstPersonController()
-
-player.gravity = 0.65
-ZombieModel = load_model('zombie.obj')
-zombie1 = Entity(model=ZombieModel, texture=Zombie, scale=1, double_sided=True, y=1,)
+# zombie1 = Entity(model=ZombieModel, texture=Zombie, scale=1, double_sided=True, y=1, )
 
 # app.run()
